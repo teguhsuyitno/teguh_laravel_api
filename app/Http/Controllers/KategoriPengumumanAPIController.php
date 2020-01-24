40a7762a349f152536ad51e661112abb326a767a
teguh <55232749+teguhsuyitno@users.noreply.github.com>
2020-01-24 15:42:42 +0800

    laravel_api

---
+++ app/Http/Controllers/KategoriPengumumanAPIController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\KategoriPengumuman;
+use Illuminate\Http\Request;
+
+class KategoriPengumumanAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kategoriPengumuman=KategoriPengumuman::all();
+
+        return $kategoriPengumuman;
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input=$request->all();
+
+        $kategoriPengumuman=KategoriPengumuman::create($input);
+
+        return $kategoriPengumuman;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $kategoriPengumuman=KategoriPengumuman::find($id);
+
+        return $kategoriPengumuman;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
