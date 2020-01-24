685e6b330a2b95661763ddc286eed3a059a77463
teguh <55232749+teguhsuyitno@users.noreply.github.com>
2020-01-24 15:56:02 +0800

    update berita

--- app/Berita.php
+++ app/Berita.php
@@ -7,16 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Berita extends Model
 {
     //
-}
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Berita extends Model
-{
-    //
     protected $table='berita';
 
     protected $fillable=[
